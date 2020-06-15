63bad1ffcb78754a4f7c303508c6fe065e019550
Masa-Emad <masa.hn96@gmail.com>
2020-06-15 07:33:37 +0300

    edit files

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,92 +5,92 @@ class Home extends CI_Controller {
     
 	public function index()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('index');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/index');
+        $this->load->view('books_rack/templates/footer');
 	}//index
 
 	public function accomp()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('accomp');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/accomp');
+        $this->load->view('books_rack/templates/footer');
 	}//accomp
 
 	public function articles()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('articles');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/articles');
+        $this->load->view('books_rack/templates/footer');
 	}//articles
 
 	public function articleView()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('articleView');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/articleView');
+        $this->load->view('books_rack/templates/footer');
 	}//articleView
 
 	public function bookDesc()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('bookDesc');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/bookDesc');
+        $this->load->view('books_rack/templates/footer');
 	}//bookDesc
 
 	public function bookSearch()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('bookSearch');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/bookSearch');
+        $this->load->view('books_rack/templates/footer');
 	}//bookSearch
 
 	public function bookshelf()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('bookshelf');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/bookshelf');
+        $this->load->view('books_rack/templates/footer');
 	}//bookshelf
 
 	public function display()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('display');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/display');
+        $this->load->view('books_rack/templates/footer');
 	}//display
 
 	public function donate()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('donate');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/donate');
+        $this->load->view('books_rack/templates/footer');
 	}//donate
 
 	public function join_us()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('join_us');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/join_us');
+        $this->load->view('books_rack/templates/footer');
 	}//join_us
 
 	
 
 	public function telegram()
 	{
-		$this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('telegram');
-        $this->load->view('templates/footer');
+		$this->load->view('books_rack/templates/header');
+        $this->load->view('books_rack/templates/navbar');
+        $this->load->view('books_rack/telegram');
+        $this->load->view('books_rack/templates/footer');
 	}//telegram
 
 	
--- application/controllers/management_book.php
+++ application/controllers/management_book.php
@@ -1,48 +1,62 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class management_book extends CI_Controller {
+class Management_book extends CI_Controller {
     
 	public function index()
 	{
-		
+        $data['title'] = 'Management Book';
+		$this->load->view('management_book/templates/header', $data);
         $this->load->view('management_book/management');
+        $this->load->view('management_book/templates/footer');
 	}
 	public function add_book()
 	{
-		
-
-            $this->load->view('management_book/add_book');
-        	}
+		$data['title'] = 'Add Book';
+		$this->load->view('management_book/templates/header', $data);
+        $this->load->view('management_book/add_book');
+        $this->load->view('management_book/js/add_book_js');
+        $this->load->view('management_book/templates/footer');
+    }
 
     public function show_book()
     {
-       
-            $this->load->view('management_book/show_book');
-        }
-        public function change_pic()
-        {
-            $this->load->view('management_book/change_pic');
+        $data['title'] = 'Show Book';
+		$this->load->view('management_book/templates/header', $data);
+        $this->load->view('management_book/show_book');
+        $this->load->view('management_book/templates/footer');
+    }
+    
+    public function change_pic()
+    {
+        $this->load->view('management_book/change_pic');
 
-        }
-         public function show_article()
-        {
-            $this->load->view('management_book/show_article');
+    }
+    
+    public function show_article()
+    {
+        $this->load->view('management_book/show_article');
 
-        }
-         public function add_activity()
-        {
-            $this->load->view('management_book/add_activity');
+    }
+    
+    public function add_activity()
+    {
+        $this->load->view('management_book/add_activity');
 
-        }
-         public function add_article()
-        {
-            $this->load->view('management_book/add_article');
+    }
+    
+    public function add_article()
+    {
+        $this->load->view('management_book/add_article');
 
-        }
-        public function add_infographic()
-        {
-            $this->load->view('management_book/add_infographic');
+    }
+    
+    public function add_infographic()
+    {
+        $data['title'] = 'Add Infographic';
+		$this->load->view('management_book/templates/header', $data);
+        $this->load->view('management_book/add_infographic');
+        $this->load->view('management_book/templates/footer');
 
-        }
+    }
 }
