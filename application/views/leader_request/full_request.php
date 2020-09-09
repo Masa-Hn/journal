e0f0e0c81058e2fce68597510b594a62eb4d20f4
asmaahamid99 <asmaa55hm@gmail.com>
2020-09-09 14:07:17 +0300

    add edit button

--- application/views/leader_request/full_request.php
+++ application/views/leader_request/full_request.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-	<title>Osboha 180</title>
-
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/request.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js" type="text/javascript"></script>
-
-</head>
 
 <body>
 
@@ -32,7 +17,7 @@
 			<div class="modal-content">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h1 class="modal-title"> ﺻﻔﺤﺔ ﻃﻠﺐ ﺳﻔﺮاء ﺟﺪﺩ </h1>
+					<h1 class="modal-title"> طﻠﺐ ﺳﻔﺮاء ﺟﺪﺩ </h1>
 				</div>
 
 				<div class="modal-body">
@@ -91,12 +76,13 @@
 				</div>
 
 				<div class="modal-footer">
-					<button type="button" class="btn btn-default close-btn" data-dismiss="modal">إغلاق</button>
-				</div>
+					<button type="button" class="btn btn-default close-btn" data-dismiss="modal">إغلاق </button>	
+			</div>
 
 			</div>
 		</div>
 	</div>
+
 	<script type="text/javascript">
 		var base_url = "<?php echo base_url()?>";
 		$( document ).ready( function () {
