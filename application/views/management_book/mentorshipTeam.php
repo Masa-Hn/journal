397c0928fdbcf195a88634bf8bb4fa44080bea4b
asmaahamid99 <asmaa55hm@gmail.com>
2020-09-19 12:28:31 +0300

    adding search

--- application/views/management_book/mentorshipTeam.php
+++ application/views/management_book/mentorshipTeam.php
@@ -9,6 +9,19 @@
     <div class="row d-flex justify-content-center">
         <div class="col-xl-10 col-lg-10 col-md-10">
             <div class="card b-0" style="overflow-x: auto;">
+              <form class="s-form" enctype="multipart/form-data" method="post" action="<?php echo base_url();?>/MentorshipTeam2/searchRequest">
+                <div class="row">
+                  <div class="col-md-7">
+                    <input class="form-control" name="s-text" id="s-text" type="text" placeholder="ابحث عن طريق اسم القائد/السفير أو التاريخ">
+                  </div>
+                  <div class="col-md-3">
+                    <input class="form-control" name="s-date" id="s-date" type="date" placeholder="<?php echo date('Y-m-d');?>">
+                  </div>
+                  <div class="col-md-2">
+                    <input class="form-control" name="s-btn" id="s-btn" type="submit" value="بحث">
+                  </div>
+                </div>
+              </form>
               <input type="hidden" id="bookDisplay" name="bookDisplay" value="2">
               <table id="dataTable" class="cell list-wrapper" dir="rtl">
                 <thead>
@@ -17,15 +30,15 @@
                     <th>الفريق</th>
                     <th>القائد</th>
                     <th>الجنس</th>
-                    <th>عدد الأعضاء الكلي</th>
                     <th>عدد الأعضاء الجدد</th>
+                    <th>تاريخ الطلب</th>
                   </tr>
                 </thead>
                 <tbody>
                   <?php
                     foreach ($requests->result() as $request) {
                       $id = $request->Rid;
-                      $ambassadors = $this->GeneralModel->get_data($request->Rid, 'requestId', 'ambassador', 'name, gender');
+                      $ambassadors = $this->GeneralModel->get_data($request->Rid, 'requestId', 'ambassador', 'name, gender,profile_link');
                       $newMembers = $ambassadors->num_rows();
 
                   ?>
@@ -35,8 +48,8 @@
                         <svg xmlns="\http://www.w3.org/2000/svg&quot;" viewBox="0 0 80 80" focusable="false"><path d="M70.3 13.8L40 66.3 9.7 13.8z"></path></svg>
                       </button>
                     </td>
-                    <td id="lblMSb" ><?php echo $request->team_name; ?></td>
-                    <td><?php echo $request->leader_name; ?></td>
+                    <td id="lblMSb" ><a class="link" href="<?php echo $request->team_link;?>"><i class="fa fa-external-link" aria-hidden="true"></i><?php echo $request->team_name; ?></a></td>
+                    <td><a class="link" href="<?php echo $request->leader_link;?>"><i class="fa fa-external-link" aria-hidden="true"></i><?php echo $request->leader_name; ?></a></td>
                     <td><?php
                       if($request->gender == 'Female' || $request->gender == 'female'){
                         echo "أنثى";
@@ -44,8 +57,9 @@
                         echo "ذكر";
                       }
                     ?></td>
-                    <td><?php $totalMembers=20; echo $totalMembers;?></td>
+
                     <td><?php echo $newMembers;?></td>
+                    <td><?php echo date('Y-m-d', strtotime($request->date));?></td>
                   </tr>
                     <?php
                       foreach ($ambassadors->result() as $ambassador) {
@@ -53,7 +67,7 @@
                   <tr id="<?php echo 'MS01b'.$id;?>" class="hidden">
                     <td></td>
                     <td></td>
-                     <td class="<?php echo 'MS02b'.$id;?>"><?php echo $ambassador->name;?></td>
+                     <td class="<?php echo 'MS02b'.$id;?>"><a class="link" href="<?php echo $ambassador->profile_link;?>"><i class="fa fa-external-link" aria-hidden="true"></i><?php echo $ambassador->name;?></a></td>
                     <td><?php
                     if($ambassador->gender == 'Female' || $ambassador->gender == 'female'){
                       echo "أنثى";
