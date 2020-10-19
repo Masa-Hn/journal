042cdf2826f95d93696fe28f31a1d92d3709857e
Shatha95Alassady <Shatha.ite@gmail.com>
2020-10-19 14:57:48 +0300

    edit members permissions

--- application/controllers/EditUsers.php
+++ application/controllers/EditUsers.php
@@ -1,7 +1,7 @@
 <?php
 defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
 
-class AddUsers extends CI_Controller {
+class EditUsers extends CI_Controller {
 
 	public
 
@@ -17,8 +17,8 @@ public function index() {
 		$this->load->library("pagination");
 
 		$config = array();
-		$config["base_url"] = base_url() . "AddUsers";
-		$config["total_rows"] = $this->GeneralModel->get_data(0,'regstatus','users')->num_rows();
+		$config["base_url"] = base_url() . "EditUsers";
+		$config["total_rows"] = $this->GeneralModel->get_data(1,'regstatus','users')->num_rows();
 		$config["per_page"] = 5;
 		$config["uri_segment"] = 2;
 		$config['full_tag_open'] = '<ul class="pagination justify-content-center" >';
@@ -35,7 +35,7 @@ public function index() {
 		$config['next_tag_close'] = '</li>';
 		$config['prev_link'] = '&larr;';
 		$config['prev_tag_open'] = '<li class="page-item" style="border:solid 1px #A52A2A">';
-		$config['prev_tag_close'] = '</li>'; 
+		$config['prev_tag_close'] = '</li>';
 		$config['cur_tag_open'] = '<li class="page-item active"><a href="#" class="page-link" style="background-color :#A52A2A;border:solid 1px #A52A2A;">';
 		$config['cur_tag_close'] = '<span class="sr-only">(current)</span></a></li>';
 		$config['num_tag_open'] = '<li class="page-item" style="border:solid 1px #A52A2A">';
@@ -44,24 +44,41 @@ public function index() {
 		$this->pagination->initialize($config);
 		$page = ($this->uri->segment(2)) ? $this->uri->segment(2) :0;
 		$data["links"] = $this->pagination->create_links();
-		$data['title'] = 'Accept Members';
-		$data['members']=$this->GeneralModel->get_data_limit(0,'regstatus','users',$config["per_page"], $page);
+		$data['title'] = 'Edit Members';
+		$data['members']=$this->GeneralModel->get_data_limit(1,'regstatus','users',$config["per_page"], $page);
+					
 		$this->load->view('management_book/templates/header',$data);
 		$this->load->view('management_book/templates/navbar');
-		$this->load->view( 'management_book/AddUsers',$data );
+		$this->load->view( 'management_book/EditUsers',$data );
 		$this->load->view('management_book/templates/footer');
-		}
+				}
 
-function add(){
-		$username=$this->input->post('name');
-		$data['regstatus']=1;
-		$this->GeneralModel->update($data,$username,'users','username');
-		redirect(base_url().'AddUsers/index');
-		}
+function edit(){
+		  $teams = $_POST['team'];
+		  $str="";
+		$id=$this->input->post('id');
+		 if(!empty($teams)) 
+		  {
+		    $N = count($teams);
+		    for($i=0; $i < $N; $i++)
+		    {
+		    	if ($i!=$N-1)
+		      $str=$str.$teams[$i]."+";
+		  if ($i==$N-1)
+		      $str=$str.$teams[$i];
 
-function delete(){	
+		    }
+			mb_substr($str ,0, -1);		
+		  }
+		  $data['team']=$str;
+		  $this->GeneralModel->update($data,$id,'users');
+
+		redirect(base_url().'EditUsers/index');
+				}
+
+function delete(){
 		$id=$this->input->post('id');
 		$this->GeneralModel->remove($id,'users','id');
-		redirect(base_url().'AddUsers/index');
-		}
-}
+		redirect(base_url().'EditUsers/index');
+				}
+} 
