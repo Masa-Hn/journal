29a22247e6fe0d501ed02af330be73fef129daf3
saraIsmailSE <saraismailse@gmail.com>
2020-11-05 13:21:34 +0300

    done

--- application/views/sign_up/fb_login.php
+++ application/views/sign_up/fb_login.php
@@ -5,8 +5,10 @@
     $page_id = 12;
     $this->StatisticsModel->addVisitor($page_id);
 ?>
+
+
 <!-- Start Banner Area -->
-    <section class="banner-area relative bgImg2">
+    <section id='1' class="banner-area relative bgImg2">
       <div class="container">
         <div class="row fullscreen align-items-center justify-content-center">
           <div class="banner-left col-lg-8 col-sm-12 story-content left-text" dir="rtl">
@@ -29,8 +31,58 @@
     </section>
 <!-- End Banner Area -->
 
+    <section id='2' class="banner-area relative bgImg2" style="display: none;">
+      <div class="container">
+        <div class="row fullscreen align-items-center justify-content-center">
+          <div class="banner-left col-lg-12 col-sm-12 story-content left-text text-center " dir="rtl">
+            <h1>
+              مرحبًا بك  
+              <a id='fbLink'>
+                <span class="sp-2" id="name" >
+                
+                </span>
+              </a>
+              <br/>
+              <br/>
+              <div style="color: black;
+                  margin: 0 auto;
+                  width: 25%;
+                  opacity: 0.8;
+                  background: #aeceba;
+                  font-size: 15px;">
+
+                  للتأكيد، هذه معلوماتك التي زودنا بها الفيسبوك
+
+                <br>
+                <span id="email"></span>
+                <br/>
+                <span id="gender"></span>
+                <br/>
+                <span id="profile">
+                  <a id="userfbLink">Facebook profile</a>
+                </span>
+              </div>
+            </h1>
+
+            <br>
+            <h1 class="sp-1">
+              أهلًا بك معنا            
+            </h1>
+
+            <a href="https://www.facebook.com/groups/1518939228167551/" class="final-page genric-btn primary circle arrow" target="_blank">
+              اضغط هنا للانضمام لفريق القراءة الخاص بك
+            </a>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
 <!-- Load the JS SDK asynchronously -->
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
 </body>
-<script type="text/javascript" src="<?php echo base_url()?>assets/sign_up_assests/js/fbLogin.js"></script>
+<script type="text/javascript" src="<?php echo base_url()?>assets/sign_up_assests/js/fbLogin_2.js"></script>
 <?php include 'templates/footer.php';?>
