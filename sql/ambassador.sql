7780528f8569111e1a7198733ce4b1a0ac7065e9
Masa-Emad <masa.hn96@gmail.com>
2020-12-04 05:03:09 +0300

    add user to mark list

--- sql/ambassador.sql
+++ sql/ambassador.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 17, 2020 at 11:11 PM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 5.6.36
+-- Host: localhost:3306
+-- Generation Time: Dec 04, 2020 at 05:00 AM
+-- Server version: 5.7.32-cll-lve
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `journal`
+-- Database: `osbohaco_rack`
 --
 
 -- --------------------------------------------------------
@@ -37,10 +37,11 @@ CREATE TABLE `ambassador` (
   `request_id` int(11) DEFAULT NULL,
   `profile_link` varchar(255) NOT NULL,
   `fb_id` varchar(255) NOT NULL,
-`messenger_id` varchar(255) NOT NULL DEFAULT '0',
+  `messenger_id` varchar(255) NOT NULL DEFAULT '0',
   `is_joined` tinyint(1) NOT NULL DEFAULT '0',
   `join_following_team` int(11) NOT NULL DEFAULT '0',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `display` tinyint(2) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -62,7 +63,7 @@ ALTER TABLE `ambassador`
 -- AUTO_INCREMENT for table `ambassador`
 --
 ALTER TABLE `ambassador`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- Constraints for dumped tables
