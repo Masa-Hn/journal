f79eeab26940c5fc37bb948bbdfb27f5bcddbe55
Masa-Emad <masa.hn96@gmail.com>
2020-12-15 06:12:45 +0300

    edit

--- application/controllers/newMembersList.php
+++ application/controllers/newMembersList.php
@@ -18,26 +18,28 @@ class NewMembersList extends CI_Controller {
 
 		if ( $leader_info->num_rows > 0 ) {
 			$res = $leader_info->fetch_array( MYSQLI_ASSOC );
-			$id = $res[ 'id' ];
-			$request = $this->requests_model->leaderLastRequest( $id, 'leader_id', 'leader_request', '*' );
-			if ( $request->num_rows > 0 ) {
-				$request_info = $request->fetch_array( MYSQLI_ASSOC );
-				$Rid = $request_info[ 'Rid' ];
-				$arr[ 'leader_id' ] = $id;
-				$arr[ 'uniqid' ] = $res[ 'uniqid' ];
-				$arr[ 'leader_name' ] = $res[ 'leader_name' ];
-				$arr[ 'team_link' ] = $res[ 'team_link' ];
-				$arr[ 'ambassadors' ] = $this->requests_model->get_data($Rid, 'request_id', 'ambassador', '*', 'AND display = 1');
+            if($res != null){
+                $id = $res[ 'id' ];
+                $request = $this->requests_model->leaderLastRequest( $id, 'leader_id', 'leader_request', '*' );
+                if ( $request->num_rows > 0 ) {
+                    $request_info = $request->fetch_array( MYSQLI_ASSOC );
+                    $Rid = $request_info[ 'Rid' ];
+                    $arr[ 'leader_id' ] = $id;
+                    $arr[ 'uniqid' ] = $res[ 'uniqid' ];
+                    $arr[ 'leader_name' ] = $res[ 'leader_name' ];
+                    $arr[ 'team_link' ] = $res[ 'team_link' ];
+                    $arr[ 'ambassadors' ] = $this->requests_model->get_data($Rid, 'request_id', 'ambassador', '*', 'AND display = 1');
 
-				if ( $arr[ 'ambassadors' ]->num_rows == 0 ) {
-					$arr[ 'info' ] = 'لم يتم التوزيع لك بعد!';
-				}
-				$this->load->view( 'leader_request/new_members_list', $arr );
-			} else {
-				$arr[ 'info' ] = "لا يوجد لديك طلبات!";
-				$this->load->view( 'leader_request/new_members_list', $arr );
-			}
-		}
+                    if ( $arr[ 'ambassadors' ]->num_rows == 0 ) {
+                        $arr[ 'info' ] = 'لم يتم التوزيع لك بعد!';
+                    }
+                    $this->load->view( 'leader_request/new_members_list', $arr );
+                } else {
+                    $arr[ 'info' ] = "لا يوجد لديك طلبات!";
+                    $this->load->view( 'leader_request/new_members_list', $arr );
+                }
+            }
+        }
 	}
 
 
