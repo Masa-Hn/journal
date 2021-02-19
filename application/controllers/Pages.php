b243e67424aea5fe54f3c781d818d28fc9f8fbe7
[Sahtha-Alassady] <[ms.messo96@gmail.com]>
2021-02-19 22:04:47 +0300

    rearrange pages and add new page to ORM

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -12,12 +12,13 @@ class Pages extends CI_Controller {
 	} //end construct()
 
 	public function index() {
-		$pages=$this->GeneralModel->get_all('pages');
+		$pages=Orm_Pages::get_all();
+		//$this->GeneralModel->get_all('pages');
 		$arr1= Array();
-		 foreach ($pages->result() as $m) { 
-        array_push($arr1,$m->id);
+		 foreach ($pages as $m) { 
+        array_push($arr1,$m->get_id());
            }
-		$data['pages']=$pages->result();
+		$data['pages']=$pages;
 		$data['title']="Rearrange Pages";
 		if (isset($_POST["page_id_array"]))
 
@@ -25,7 +26,10 @@ class Pages extends CI_Controller {
 		for($i=0; $i<count($_POST["page_id_array"]); $i++)
 			{
 				$arr['page_order']=$i;
-				$this->GeneralModel->update($arr,$_POST["page_id_array"][$i],'pages');
+				$updated=Orm_Pages::get_instance($_POST["page_id_array"][$i]);
+				$updated->set_page_order($i);
+				$updated->save();
+				//$this->GeneralModel->update($arr,$_POST["page_id_array"][$i],'pages');
 
 			}				
 
@@ -40,8 +44,11 @@ class Pages extends CI_Controller {
 	public function add() {
         
 		$title=$this->input->post('title');
-		$data['title']=$title;
-		$this->GeneralModel->insert($data,'pages');
+		//$data['title']=$title;
+		//$this->GeneralModel->insert($data,'pages');
+		$inserted=new Orm_Pages;
+		$inserted->set_title($title);
+		$inserted->save();
 		redirect(base_url().'Pages/index');
 	}
 }
