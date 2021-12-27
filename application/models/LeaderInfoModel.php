12489ca9168e997896ccbea711262d177881dd83
saraIsmailSE <saraismailse@gmail.com>
2021-12-27 06:16:55 +0300

    compleate REQ

--- application/models/LeaderInfoModel.php
+++ application/models/LeaderInfoModel.php
@@ -18,6 +18,31 @@ class LeaderInfoModel extends CI_Model {
 
 	}
 
+	public function getLeaderInfoByEmail($leader_email)
+	{
+		$this->db->where('leader_email',$leader_email);
+    	$this->db->from('leader_info');
+    	$this->db->limit(1);
+    	return $this->db->get()->row();
+
+	}
+
+	public function getReqInfo($leader_id)
+	{
+		$this->db->select('Rid as رقم الطلب , is_done as غير مكتمل , members_num as العدد المطلوب  , gender as جنس السفراء' );
+		$this->db->where('leader_id',$leader_id);
+		$this->db->where('is_done',0);
+    	$this->db->from('leader_request');
+    	return $this->db->get()->row();
+
+	}
+	public function updateRequest( $id ) {
+
+		$this->db->set( 'is_done', 1, FALSE );
+		$this->db->where( 'Rid', $id );
+		$this->db->update( 'leader_request' );
+	} //updateRequest
+
 }//LeaderInfoModel
 
 ?>
